7f8db07c44f981fa92ede222b50544179c231a4b
viniluk <viniluk9@gmail.com>
2021-07-24 11:12:19 -0300

    tratando exceções

--- src/controllers/OportunityController.js
+++ src/controllers/OportunityController.js
@@ -3,33 +3,37 @@ const Oportunity = require("../models/Oportunity");
 
 module.exports = {
   async store(req, res) {
-    const response = await axios.get(
-      `${process.env.URL_PIPEDRIVE}${process.env.TOKEN_PIPEDRIVE}`
-    );
+    try {
+      const response = await axios.get(
+        `${process.env.URL_PIPEDRIVE}${process.env.TOKEN_PIPEDRIVE}`
+      );
 
-    const arr = response.data.data;
+      const arr = response.data.data;
 
-    // PRA CADA RESPOSTA ELE INSERE A OPORTUNIDADE NO BANCO
-    arr.map(async function (responseArray) {
-      await Oportunity.create({
-        oportunity_id: responseArray.id,
-        value: responseArray.value,
-        day: responseArray.add_time,
+      // PRA CADA RESPOSTA ELE INSERE A OPORTUNIDADE NO BANCO
+      arr.map(async function (responseArray) {
+        await Oportunity.create({
+          oportunity_id: responseArray.id,
+          value: responseArray.value,
+          day: responseArray.add_time,
+        });
       });
-    });
 
-    return res.json({
-      message: "Oportunidades agregadas com sucesso!",
-    });
+      return res.json({
+        message: "Oportunidades agregadas com sucesso!",
+      });
+    } catch (error) {
+      return res.json({ error });
+    }
   },
 
   async index(req, res) {
-    const oportunitys = await Oportunity.find();
+    try {
+      const oportunitys = await Oportunity.find();
 
-    return res.json({
-      Opotunitys: {
-        oportunitys,
-      },
-    });
+      return res.json({ oportunitys });
+    } catch (error) {
+      return res.json({ error });
+    }
   },
 };
